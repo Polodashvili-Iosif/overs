b7b72dd41bd8703018dd97ea48a4549e197e11da
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2023-06-25 09:16:20 +0300

    Заменена главная страница, на страницу с задачами

--- overs/pages/urls.py
+++ overs/pages/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('', views.index, name='index'),
     path('', views.index, name='index'),
     path('orders/', views.orders, name='orders'),
-    path('tasks/', views.tasks, name='tasks'),
     path('plan/', views.plan, name='plan'),
     path('my_plan/', views.my_plan, name='my_plan'),
+    path('map/', views.map, name='map'),
 ]
--- overs/pages/views.py
+++ overs/pages/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'pages/index.html')
+# def index(request):
+#     return render(request, 'pages/index.html')
 
 
 def orders(request):
     return render(request, 'pages/orders.html')
 
 
-def tasks(request):
+def index(request):
     return render(request, 'pages/tasks.html')
 
 
 def plan(request):
     return render(request, 'pages/plan.html')
 
+
 def my_plan(request):
     return render(request, 'pages/my_plan.html')
+
+
+def map(request):
+    return render(request, 'pages/map.html')
